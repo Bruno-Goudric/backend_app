0b23fc2d902c7cbe08d358f942bede6f5ab4173b
Bruno-Goudric <brunolfsports@hotmail.com>
2020-08-27 08:50:51 -0300

    Create a first Test unitary

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,13 +1,22 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+describe('CreateAppointmentService', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+    expect(appointment).toHaveProperty('id');
   });
 });
 
-describe('CreateAppointment', () => {
-  it('should not be able to create two appointments on the same time', () => {
-    expect(1 + 2).toBe(3);
-  });
-});
+// describe('CreateAppointment', () => {
+//   it('should not be able to create two appointments on the same time', () => {
+//     expect(1 + 2).toBe(3);
+//   });
+// });
