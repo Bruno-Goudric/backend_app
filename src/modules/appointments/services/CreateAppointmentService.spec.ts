bccaa935b5a36ef2d4994436c45215a342b232cb
Bruno-Goudric <brunolfsports@hotmail.com>
2020-09-01 11:23:20 -0300

    Create a Teste Unitary about Authenticate/CreateUser

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -12,11 +13,27 @@ describe('CreateAppointmentService', () => {
       provider_id: '123123123',
     });
     expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
   });
 });
 
-// describe('CreateAppointment', () => {
-//   it('should not be able to create two appointments on the same time', () => {
-//     expect(1 + 2).toBe(3);
-//   });
-// });
+describe('CreateAppointment', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointmentDate = new Date(2020, 5, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123',
+    });
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
